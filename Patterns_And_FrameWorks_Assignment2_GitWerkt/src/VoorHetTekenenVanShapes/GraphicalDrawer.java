4e608782ae8462118bde6b25d15be67db0c0b410
jbsleepz <kotshond@gmail.com>
2016-12-27 11:40:13 +0100

    testten

--- Patterns_And_FrameWorks_Assignment2_GitWerkt/src/VoorHetTekenenVanShapes/GraphicalDrawer.java
+++ Patterns_And_FrameWorks_Assignment2_GitWerkt/src/VoorHetTekenenVanShapes/GraphicalDrawer.java
@@ -253,7 +253,7 @@ public class GraphicalDrawer extends javax.swing.JFrame implements ActionListene
 				completeTrain = new CompleteTrain(locomotief);
 
 				// ipv addTrain uit deze klasse moet hij worden toegevoegd worden aan de klasse TrainStations
-				// Daarnaast moet de arraylist van Trainstations worden ingelezen worden in de combobox. test
+				// Daarnaast moet de arraylist van Trainstations worden ingelezen worden in de combobox. test test
 				addTrain(waardeNieuweTrein);
 				cbAllTrains.getSelectedIndex();
 				shapeDraw.drawShapeObject(trainShape, waardeNieuweTrein, drawPanel);
