b91261a5b4c8da232e21148d5389da2a753e2f88
Daphne Hogeterp <daphne_hogeterp@hotmail.com>
2016-12-27 14:37:47 +0100

    commands

--- Patterns_And_FrameWorks_Assignment2_GitWerkt/src/Controller_command/RemCommand.java
+++ Patterns_And_FrameWorks_Assignment2_GitWerkt/src/Controller_command/RemCommand.java
@@ -1,11 +1,13 @@
 package Controller_command;
 
+import LibariesImport.Program;
+
 public class RemCommand extends CommandType{
 	// Doet Daphne
 	@Override
 	public void execute(String input) {
 		// TODO Auto-generated method stub
-		
+		Program.between(input, "remove", "from");
 	}
 
 }
