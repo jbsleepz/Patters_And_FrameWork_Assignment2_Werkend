351671b52be8582063ff9fef56120e158cda0b8d
Daphne Hogeterp <daphne_hogeterp@hotmail.com>
2016-12-27 17:36:21 +0100

    commands

--- Patterns_And_FrameWorks_Assignment2_GitWerkt/src/Controller_command/NewWagonCommand.java
+++ Patterns_And_FrameWorks_Assignment2_GitWerkt/src/Controller_command/NewWagonCommand.java
@@ -1,13 +1,39 @@
 package Controller_command;
 
+import java.util.ArrayList;
+
+import Domain.CompleteTrain;
+import Domain.PassengerWagon;
+import Domain.TrainStation;
 import Domain.Wagon;
 import LibariesImport.Program;
 
 public class NewWagonCommand extends CommandType{
+	
+	TrainStation trains;
+	CompleteTrain completeTrain;
 	// Doet Daphne
 	@Override
 	public void execute(String input) {
 		String wagonID = Program.after(input, "wagon");
+		
+		ArrayList<CompleteTrain> train = trains.getCompleteTrains();
+		
+		for (CompleteTrain completeTrain : train) {
+				ArrayList<Wagon> wagons = completeTrain.getWagons();
+				for (Wagon wagon : wagons){
+					if(wagon.getName().equals(wagonID)){
+						System.out.println("het meegegeven ID bestaat al");
+						break;
+						} 
+					else 
+						{
+						new PassengerWagon(wagonID, 20);
+						break;
+						}
+				}
+			}
+		
 		new Wagon(wagonID);
 	}
 
