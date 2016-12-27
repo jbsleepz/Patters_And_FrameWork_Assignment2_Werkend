2fe54e22c2c006484e6491476722728d1f47e6c7
jbsleepz <kotshond@gmail.com>
2016-12-27 15:29:49 +0100

    Hopelijk werkt dit

--- Patterns_And_FrameWorks_Assignment2_GitWerkt/src/Controller_command/RemCommand.java
+++ Patterns_And_FrameWorks_Assignment2_GitWerkt/src/Controller_command/RemCommand.java
@@ -5,20 +5,30 @@ import java.util.ArrayList;
 import Domain.CompleteTrain;
 import LibariesImport.Program;
 import Domain.TrainStation;
+import Domain.Wagon;
 
 public class RemCommand extends CommandType {
+	TrainStation trains;
+	CompleteTrain trainWagon;
 	// Doet Daphne
 	@Override
 	public void execute(String input) {
 		String WagonID = Program.between(input, "remove", "from");
 		String TrainID = Program.after(input, "from");
 
-		TrainStation trains;
 		ArrayList<CompleteTrain> train = trains.getCompleteTrains();
+		ArrayList<Wagon> wagons = trainWagon.getWagons();
 
 		for (CompleteTrain completeTrain : train) {
 			if (completeTrain.getLocomotive().equals(TrainID)) {
-				completeTrain.removeWagons(WagonID);
+				for (Wagon wagon : wagons){
+					if(wagon.getName().equals(WagonID)){
+						completeTrain.removeWagons(WagonID);
+					}
+				}
+				
+				
+				
 			}
 		}
 
