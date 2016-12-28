ead7339a7489d73f142bf49ecf0950625d642986
jbsleepz <kotshond@gmail.com>
2016-12-28 16:23:18 +0100

    Domain geupdated

--- Patterns_And_FrameWorks_Assignment2_GitWerkt/src/Controller_Commands/del_command.java
+++ Patterns_And_FrameWorks_Assignment2_GitWerkt/src/Controller_Commands/del_command.java
@@ -1,13 +1,29 @@
 package Controller_Commands;
 
-public class del_command extends CommandType{
-	//doet jb
-	//delete train tr1; // response: “train tr1 deleted”
-	//delete train tr2; // response: “train tr2 does not exist”
+public class del_command extends Command {
+	// doet jb
+	// delete train tr1; // response: “train tr1 deleted”
+	// delete train tr2; // response: “train tr2 does not exist”
+	// token[0] is altijd de class name
 	@Override
-	public void execute(String input) {
-		// TODO Auto-generated method stub
-		
+	public boolean execute() {
+		boolean b = true;
+
+		if (characters[1].equals("wagon") || characters[1].equals("train")
+				|| super.GeldigheidCommandcontrole("[a-zA-Z0-9]*", characters[2])) {
+
+			if (characters[1].equals("train")) {
+				if (train.TrainExists(characters[2])) {
+
+				}
+			if (characters[1].equals("wagon")){
+				train.removeTrain(train);
+			}
+
+			}
+		}
+
+		return b;
 	}
 
 }
