f27b49704ebb411a159dd314f579f3fd4bd2e65d
Daphne Hogeterp <daphne_hogeterp@hotmail.com>
2016-12-28 17:07:52 +0100

    aanpassingen newWagon_command

--- Patterns_And_FrameWorks_Assignment2_GitWerkt/src/Controller_Commands/newWagon_command.java
+++ Patterns_And_FrameWorks_Assignment2_GitWerkt/src/Controller_Commands/newWagon_command.java
@@ -15,29 +15,30 @@ public class newWagon_command extends Command{
 	TrainStation trains;
 	CompleteTrain completeTrain;
 	
+	// example command: "new wagon <<WagonID>> numseats <<NUMBER>>" or "new wagon <<WagonID>> maxweight <<NUMBER>>"
 	@Override
 	public boolean execute() {
 		
 		boolean b = true;
 		
-		if (characters[0].equals("new") && characters[1].equals("train")|| super.GeldigheidCommandcontrole("[a-z0-9]*", characters[2])) {
-			Locomotive locomotive = new Locomotive();
-			locomotive.setname(characters[2]);
-			CompleteTrain completetrain = new CompleteTrain(locomotive);
-			if (characters[2].equals("goods")) {
-				Wagon w = new GoodsWagon(characters[1], 1000);
-				completetrain.addWagons(w);
-				super.setErrorMessage("er is een goederen wagon aan trein" + locomotive.toString() + " toegevoegd /n");
-			} else if (characters[2].equals("passenger")) {
-				Wagon w = new PassengerWagon(characters[1], 20);
-				completetrain.addWagons(w);
-				super.setErrorMessage("er is een passagiers wagon aan trein" + locomotive.toString() + " toegevoegd /n");
+		if (characters[0].equals("new") && characters[1].equals("wagon")|| super.GeldigheidCommandcontrole("[a-z0-9]*", characters[2])) {
+			if (characters[3].equals("maxweight")) {
+				String sMaxWeight = characters[4];
+				int iMaxWeight = Integer.parseInt(sMaxWeight.trim());	
+				Wagon w = new GoodsWagon(characters[2], iMaxWeight);
+				super.setErrorMessage("er is een goederen wagon " + characters[2] + " aangemaakt /n");
+			} else if (characters[3].equals("numseats")) {
+				String sAmountPassengers = characters[4];
+				int iAmountPassengers = Integer.parseInt(sAmountPassengers.trim());	
+				Wagon w = new PassengerWagon(characters[2], iAmountPassengers);
+				super.setErrorMessage("er is een passagiers wagon " + characters[2] + " aangemaakt /n");
 			} else {
-				super.setErrorMessage("Het type is verkeerd meegegeven, example wg1 Passenger to tr1 /n");
-				b = false;
+				Wagon w = new PassengerWagon(characters[2], 20);
+				super.setErrorMessage("er is een passagiers wagon " + characters[2] + " aangemaakt /n");
 			}
 		} else {
 			super.setErrorMessage("De command is verkeerd meegegeven, example wg1 Passenger to tr1 /n");
+			b = false;
 		}
 		return b;
 		
